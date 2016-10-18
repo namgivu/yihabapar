d8a601f685be108e3d9e078aa39cfe9dc56ffb74
namgivu <namgivu@gmail.com>
2016-10-18 17:55:47 +0700

    all from one file as simplicity

--- let-me-in.sh
+++ let-me-in.sh
@@ -16,8 +16,27 @@ wget $u -O $o ; unzip $o -d $t ;
 #copy to destination & clean up temp
 mv $t/yihabapar-master/* $UTIL_HOME ; rm -rf $t ;
 
-#set up path on destination site
-$UTIL_HOME/setup.sh
+
+#region set up path on destination site
+
+  #remove non-util assets
+  pushd `pwd` ; cd $UTIL_HOME ; rm -rf README.md LICENSE .gitirnore ; popd
+
+  #create util_config from template and clean up template
+  t="$UTIL_HOME/util_config.template.py"
+  c="$UTIL_HOME/util_config.py"
+  cp -f $t $c ; rm -rf $t
+
+  #get app home, app name ref. http://unix.stackexchange.com/a/6470/17671
+  APP_HOME=`git rev-parse --show-toplevel`
+  APP_NAME=`basename $APP_HOME`
+
+  #fill in util_config ref. http://stackoverflow.com/a/525612/248616
+  s="path/to/app/home" ; sed -i-e "s#$s#$APP_HOME#g" $c
+  s="YOUR-APP-NAME"    ; sed -i-e "s#$s#$APP_NAME#g" $c
+
+#endregion set up path on destination site
+
 
 #print the outcome
 echo ; echo $UTIL_HOME ; ls -lA $UTIL_HOME
