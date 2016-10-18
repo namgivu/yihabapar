f63772dd2bb8e311d055a24d89b18ccf336d4858
namgivu <namgivu@gmail.com>
2016-10-18 17:51:13 +0700

    add setup step

--- let-me-in.sh
+++ let-me-in.sh
@@ -2,14 +2,22 @@
 #ref. http://unix.stackexchange.com/a/2691/17671
 
 UTIL_HOME="/path/to/UTIL_HOME"
-UTIL_HOME="/home/namgivu/NN/code/django-start/util"
-#UTIL_HOME="./util"
 
 u="https://github.com/namgivu/yihabapar/archive/master.zip" ;
 t="/tmp/namgivu.yihabapar" ;
 o="$t/master.zip" ;
 
+#prepare temp folder
 rm -rf $t ; mkdir -p $t ;
+
+#download & unpack code
 wget $u -O $o ; unzip $o -d $t ;
+
+#copy to destination & clean up temp
 mv $t/yihabapar-master/* $UTIL_HOME ; rm -rf $t ;
+
+#set up path on destination site
+$UTIL_HOME/setup.sh
+
+#print the outcome
 echo ; echo $UTIL_HOME ; ls -lA $UTIL_HOME
