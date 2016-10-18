c43f466af577ab1a2e2fa1ff6e2406d3d2d4e54e
namgivu <namgivu@gmail.com>
2016-10-18 17:51:25 +0700

    util_config template

--- util_config.template.py
+++ util_config.template.py
@@ -1,6 +1,6 @@
 config={}
 
 config.update(APP_NAME='YOUR-APP-NAME') #used as container folder in /tmp folder for some tasks
-config.update(APP_HOME='path/to/some/file/at/root/level') #the root folder of your application
+config.update(APP_HOME='path/to/app/home') #the root folder of your application
 
 util_config=config
