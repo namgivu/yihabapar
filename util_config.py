2acf336db2d4e18b8a74869638833f0679f93bc5
namgivu <namgivu@gmail.com>
2016-10-18 16:26:34 +0700

    remove util_config.py as ignored

--- util_config.py
+++
@@ -1,6 +0,0 @@
-config={}
-
-config.update(APP_NAME='YOUR-APP-NAME') #used as container folder in /tmp folder for some tasks
-config.update(APP_HOME='path/to/some/file/at/root/level') #the root folder of your application
-
-util_config=config
