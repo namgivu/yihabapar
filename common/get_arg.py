ce9585bd318855019ed29ee284e6d67b0bea529c
namgivu <namgivu@gmail.com>
2016-10-19 14:18:40 +0700

    get_arg requires all args list

--- common/get_arg.py
+++ common/get_arg.py
@@ -4,11 +4,17 @@ Parse command line arguments
 import argparse  # ref. https://docs.python.org/2/howto/argparse.html
 
 
-def get_arg(argName, default=None):
-  value = default
+def get_arg(argName, allArgs=[], default=None):
+  if not argName:
+    return None
+
+  value=default
 
   parser=argparse.ArgumentParser()
-  parser.add_argument(argName)
+
+  for arg in allArgs:
+    parser.add_argument(arg)
+
   args=parser.parse_args()
   if hasattr(args, argName):
     value=getattr(args, argName)
